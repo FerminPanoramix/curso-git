e5413600e07e313ce90c9b72c2b6dd10863a568a
Faustino-master-uca <faustino.martin@gm.uca.es>
2025-05-02 13:51:46 +0200

    Hola nuevo commit:

--- hellogit.py
+++ hellogit.py
@@ -4,3 +4,4 @@ print("Ahora estoy editando el fichero")
 print("Vuelvo a hacer más cambios en hellogit")
 # Ahora voy a probar la herramienta diff
 print("New hellogit") #modificaciones al fichero.
+# meto nuevos comentarios
